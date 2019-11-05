762e93922b7a603f924ecd2ea1173d88c3011a02
Jackson Oppenheim <jacksonopp@gmail.com>
2019-11-05 11:05:33 -0600

    removed app.js and index.html

--- probable-winner/src/components/StudentButton.js
+++ probable-winner/src/components/StudentButton.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const getRandomStudent = () => {
+  const studentList = [
+    'student one',
+    'student two',
+    'student three',
+    'student four',
+    'student five',
+    'student six',
+  ];
+
+  const randomNumber = Math.floor(Math.random() * studentList.length);
+
+  return randomNumber()
+}
 
 export default function StudentButton() {
 
-  const [student, setStudent] = useState("one")
+  const [student, setStudent] = useState("")
+  useEffect(() => {
+    setStudent(getRandomStudent())
+  }, [])
 
   return (
     <>
       <button
-        onClick={() => { student === "one" ? setStudent("two") : setStudent("one") }}
+        onClick={() => { setStudent(getRandomStudent()) }}
       >
         {student}
       </button>
